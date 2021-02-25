874a45ac830e6c6e1e372b112e10d9dd987a1107
Taras <tkoval37@gmail.com>
2021-02-25 18:58:13 +0200

    add remote database connection

--- config/db.php
+++ config/db.php
@@ -1,8 +1,16 @@
 <?php
 
-return [
+/*return [
     'host' => 'localhost',
     'db' => 'todo',
     'user' => 'root',
     'pass' => '1111'
+];*/
+
+// Remote database connection
+return [
+    'host' => 'remotemysql.com',
+    'db' => 'YV5TzmO1fD',
+    'user' => 'YV5TzmO1fD',
+    'pass' => 'KZ3ydYhwiG'
 ];
